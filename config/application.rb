180944f6340096ece758e3b11f680429c2a933de
RicardoMatos <ricardo.vmr@gmail.com>
2015-01-28 01:57:01 -0200

    ajustes para o heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module AconteceNoCampus
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     # Heroku requires this to be false
-    config.assets.initialize_on_precompile=false
+    #config.assets.initialize_on_precompile=false
   end
 end
