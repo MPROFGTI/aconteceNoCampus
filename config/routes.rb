1cedf700bc0e3a9b8807fb81956616af53e1502a
RicardoMatos <ricardo.vmr@gmail.com>
2015-02-09 02:41:15 -0200

    Traducao telas

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   resources :eventos
 
-  resources :usuarios
-
   resources :campus
+
+  resources :usuarios
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'campus#index'
+  root 'eventos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
